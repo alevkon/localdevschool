9cfdce48e5d3a37229d9ccf1a410af6f3abf8515
finesunes <finesunes@yandex.ru>
2016-05-13 14:28:56 +0000

    mdeditor

--- assets/core/controls/mdeditor/markdownEditor.jsx
+++ assets/core/controls/mdeditor/markdownEditor.jsx
@@ -1,28 +1,12 @@
 var React = require("react");
 var Reflux = require("reflux");
 var Registry = require("../../stores/registry");
-var markdown = require( "markdown" ).markdown;
 module.exports = Registry.registerControl(React.createClass({
     displayName: "markdownEditor",
-    getInitialState() {
-        return {
-            code: this.props.value
-        };
-    },
-    onChange() {
-        this.setState({code: this.props.onChange});
-    },
-    
     render(){
-        var html_content = markdown.toHTML(this.state.code)
-        //var text = this.props.onchange;
-        
-        return <div>
-            <div className="col-md-6" value={this.state.code} onChange={this.onChange}>{this.state.code}</div>
-            <div className="col-md-6" ><div dangerouslySetInnerHTML={{__html: html_content}} /></div>
-        </div>
-                
-            
-
+        return <div class="row">
+            <div className="col-md-6">{this.props.value}</div>
+            <div className="col-md-6" ><Registry.controls.markdown source={this.props.value}/></div>
+        </div>;
     },
 }));
