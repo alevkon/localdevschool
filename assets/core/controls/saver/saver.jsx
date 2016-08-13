096950fe0c3591973c4cb892125f3746470d5dd4
AdamantHQ <adamanthq@yandex.ru>
2016-08-13 17:34:03 +0300

    Practicing in committing

--- assets/core/controls/saver/saver.jsx
+++ assets/core/controls/saver/saver.jsx
@@ -5,18 +5,45 @@ module.exports = Registry.registerControl(React.createClass({
   displayName: "saver",
 
   onClickHandler: function() {
-    var self = this;
-    var json = JSON.stringify(this.props.versions.current);
-    fetch('/api/'+ this.props.identity + '/' + this.props.id, {method: 'PUT', body: json})
-      .then(function(response) {
-        if (response.ok) {
-          self.props.upd(self.props.versions.current);
-          self.props.versions.original = self.props.versions.current;
-          self.forceUpdate();
-        } else {
-          alert('Error!')
+    var self = this, json;
+
+
+    if (this.props.create) {
+      if (!this.props.versions.current.title || !this.props.versions.current.description) {
+        alert('Please, enter Title and Description');
+
+      } else {
+        switch(this.props.identity) {
+          case 'unit':
+            this.props.versions.current['course'] = this.props.data.course.id;
+            break;
+          default:
+            break;
         }
-      });
+        json = JSON.stringify(this.props.versions.current);
+        fetch('/api/' + this.props.identity, {method: 'POST', body: json})
+          .then(function (response) {
+            if (response.ok) {
+              
+              // window.location = '/author'
+            } else {
+              alert('Error!')
+            }
+          });
+      }
+    } else {
+      json = JSON.stringify(this.props.versions.current);
+      fetch('/api/' + this.props.identity + '/' + this.props.data.id, {method: 'PUT', body: json})
+        .then(function (response) {
+          if (response.ok) {
+            self.props.upd(self.props.versions.current);
+            self.props.versions.original = self.props.versions.current;
+            self.forceUpdate();
+          } else {
+            alert('Error!')
+          }
+        });
+    }
   },
 
   render() {
